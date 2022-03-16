2532a0146984ddc6c0e7d2fb49ad6f6c547799ac
vlawrence03 <99636434+vlawrence03@users.noreply.github.com>
2022-03-16 11:35:51 -0400

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "Putchar.h"
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -6,14 +6,17 @@
  */
 int main(void)
 {
-	char *sh = "Putchar";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+	
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+	
 
 	return (0);
 }
