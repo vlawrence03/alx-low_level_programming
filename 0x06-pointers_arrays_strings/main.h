70af315277108f066feb87506a1c3412f0551e22
Vershiyi Lawrence <vershiyilawrence03@gmail.com>
2022-03-23 04:50:02 -0700

    Done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,14 @@
 
 
 char *_strcat(char *dest, char *scr);
+char *_strncat(char *dest, char *scr, int n);
+char *_strncpy(char *dest, char *scr, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+int _putchar(char c);
 
 
 #endif
