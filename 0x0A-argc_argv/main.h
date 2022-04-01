eca1621280ef96d37be1ce76a075052f5af04c67
Vershiyi Lawrence <vershiyilawrence03@gmail.com>
2022-04-01 05:27:50 -0700

    Done

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,5 +4,10 @@
 int _putchar(char c);
 int main(int _attribute_((_unused_)) argc, char *argv[]);
 int main(int argc, char _attribute_((_unused_)) *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+
 
 #endif
