df8954ed30be571abc44a8a74b6ab19cabdcefed
vlawrence03 <99636434+vlawrence03@users.noreply.github.com>
2022-04-11 15:00:03 +0100

    Update 1-pi.h

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,6 +1,12 @@
-#include "1-pi.h"
-#include "1-pi.h"
-#include <stdio.h>
+#ifndef PI_H
+#define PI_H
+
+/*
+ * File: 1-pi.h
+ * Auth: Vershiy Lawrence Kewir
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
 
 #define PI 3.14159265359
 
